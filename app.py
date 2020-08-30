7aadc6a57430a91869a1e248501202b494298918
GOSHROW <abhroindia@gmail.com>
2020-08-30 22:44:20 +0530

    Login and Autth Mechanism

--- app.py
+++ app.py
@@ -4,15 +4,18 @@ import secrets
 import flask
 import json
 import time
+import re
 
-from flask import Flask, render_template, request, flash, redirect, url_for, session
+from flask import Flask, render_template, request, flash, redirect, url_for, session, render_template_string
 import markdown.extensions.fenced_code
 from passlib.hash import sha256_crypt
 import src.LoginRegister as LR
+from src.Auth import Auth
 
 app = Flask(__name__)
 secret = secrets.token_urlsafe(32)
 app.secret_key = secret
+emailRegex = r"^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$"
 
 with open('./secret.json') as f:
     data = json.load(f)
@@ -39,31 +42,81 @@ def register_page():
         if request.method == "POST" and form.validate():
             username  = form.username.data
             email = form.email.data
-            password = sha256_crypt.encrypt((str(form.password.data)))
+            password = sha256_crypt.encrypt(str(form.password.data))
+            print(str(form.password.data))
             userCollection = client["TicketedIssues"]["Users"]
+            if not re.match(emailRegex, email):
+                flash("Email Invalid")
+                time.sleep(5)
+                return redirect(url_for("login"))
             for users in userCollection.find():
                 if users["username"].casefold() == username.casefold():
                     flash("Username is already taken, case-insensitive")
-                    return render_template('register.html', form=form)
+                    time.sleep(5)
+                    return redirect(url_for("login"))
+                if users["email"].casefold() == email.casefold():
+                    flash("Email is already taken.")
+                    time.sleep(5)
+                    return redirect(url_for("login"))
             else:
                 newUser = {
                     "username" : username,
                     "email" : email,
-                    "passHash" : password
+                    "passHash" : password, 
+                    "auth" : Auth.getAuthVal()
                 }
                 userCollection.insert_one(newUser)
                 flash("User Registered")
                 time.sleep(5)
-                session['logged_in'] = True
-                session['username'] = username
-                return redirect(url_for('/'))
+                return redirect(url_for("login"))
         return render_template("register.html", form=form)
     except Exception as e:
         return(str(e))
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/login/', methods=["GET", "POST"])
 def login():
-    pass
+    try:
+        print("A")
+        form = LR.LoginForm(request.form)
+        if request.method == "POST":
+            if form.validate():
+                username  = form.username.data
+                password = str(form.password.data)
+                userCollection = client["TicketedIssues"]["Users"]
+                for users in userCollection.find():
+                    if users["username"].casefold() == username.casefold():
+                        print("E")
+                        isUser = sha256_crypt.verify(password, users["passHash"])
+                        print(isUser)
+                        if isUser:
+                            print("F")
+                            session['logged_in'] = True
+                            session['username'] = username
+                            return redirect(url_for("auth"))
+                        else:
+                            print("G")
+                            flash("Error in login.")
+                            break
+        print("C")
+        return render_template("login.html", form=form)
+    except Exception as e:
+        return(str(e))
+
+@app.route('/auth/', methods=["GET"])
+def getAuth():
+    if 'username' in session:
+        user = session["username"]
+        userCollection = client["TicketedIssues"]["Users"]
+        for users in userCollection:
+            if users["username"] == user:
+                return render_template(users["auth"])
+        return redirect(url_for("register"))
+    else:
+        if 'logged_in' not in session or not session["logged_in"]:
+            flash("no log in")
+            time.sleep(5)
+            return redirect(url_for("login"))
+        return render_template_string("Error in getting Auth Key, check Session.")
 
 if (__name__ == '__main__'):
     app.run(debug=True)    
--- src/LoginRegister.py
+++ src/LoginRegister.py
@@ -2,7 +2,7 @@ from wtforms import Form, BooleanField, TextField, PasswordField, validators
 
 class RegistrationForm(Form):
     username = TextField('Username', [validators.Length(min=4, max=20)])
-    email = TextField('Email Address', [validators.Length(min=6, max=50)])
+    email = TextField('Email Address')
     password = PasswordField('New Password', [
         validators.Required(),
         validators.EqualTo('confirm', message='Passwords must match')
@@ -10,3 +10,7 @@ class RegistrationForm(Form):
     confirm = PasswordField('Repeat Password')
     accept_tos = BooleanField('I have ensured that my Password is unique and its leak or its hash\'s leak is not a concern of the provider: GOSHROW', [validators.Required()])
     # TODO: Got to work on ensuring safetyy
+
+class LoginForm(Form):
+    username = TextField('Username', [validators.Length(min=4, max=20)])
+    password = PasswordField('Enter Password')
